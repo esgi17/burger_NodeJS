c45b04408118be85e0cf1c00d99b51e7b2e0dabb
RobinTersou <tersou.robin@gmail.com>
2018-05-14 10:22:45 +0200

    auth

---
+++ controllers/login.js
@@ -0,0 +1,8 @@
+const ModelIndex = require('../models');
+const Login = ModelIndex.User;
+const Op = ModelIndex.sequelize.Op;
+
+const LoginController = function() { };
+
+// Export du controller
+module.exports = LoginController;
